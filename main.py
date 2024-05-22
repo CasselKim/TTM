6e7890b596dd81a15a7fe9bd96dfa8ce93afee96
CasselKim <seongr39@gmail.com>
2024-05-22 23:14:23 +0900

    test commit for cicd

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ async def test_endpoint() -> dict[str, str]:
     return {"message": "Hello, this is a test endpoint!"}
 
 
+@app.get("/test-cicd")
+async def test_cicd() -> dict[str, str]:
+    return {"message": "Hello, this is a test cicd!"}
+
+
 if __name__ == "__main__":
     import uvicorn
 

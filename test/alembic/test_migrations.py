2fd389cb0cfc5de2e1562e6f88faf6e9e39bcf6f
SEONGROK KIM <seongr39@gmail.com>
2024-05-23 01:00:38 +0900

    issue-12 make db env (#44)
    * issue-12 make db env
    
    * issue-12 add build action on github action
    
    * issue-12 add login action
    
    * issue-12 add envs for login
    
    * issue-12 add dockerfile path
    
    * issue-12 push to local registry
    
    * issue-12 fix misspell
    
    * issue-12 replace image platform
    
    * issue-12 add env for database
    
    * issue-12 remove mysql from docker

---
+++ test/alembic/test_migrations.py
@@ -0,0 +1,15 @@
+from app.core.domain.entity.user import User
+from common.database import session_scope
+
+
+def test_db_session_decorator(setup_database) -> None:
+    # insert
+    with session_scope() as session:
+        new_user = User(id=1, name="Test User", email="testemail.com")
+        session.add(new_user)
+
+    # select
+    with session_scope() as session:
+        user = session.query(User).filter_by(name="Test User").first()
+        assert user is not None
+        assert user.name == "Test User"

72e25e0f9403b7cdc16494101faf754005d051f5
rok <seongr39@gmail.com>
2025-06-10 06:50:49 +0900

    refactor: discord command external to internal

--- app/adapters/external/discord/__init__.py
+++ app/adapters/external/discord/__init__.py
@@ -1,11 +1,9 @@
 """Discord Adapter"""
 
-from app.adapters.external.discord.command_adapter import DiscordCommandAdapter
 from app.adapters.external.discord.notification_adapter import (
     DiscordNotificationAdapter,
 )
 
 __all__ = [
-    "DiscordCommandAdapter",
     "DiscordNotificationAdapter",
 ]
---
+++ app/adapters/internal/discord/__init__.py
@@ -0,0 +1,3 @@
+from .command_adapter import DiscordCommandAdapter
+
+__all__ = ["DiscordCommandAdapter"]
